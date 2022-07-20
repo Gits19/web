008c4e9a4bb94f80e155d6299f444542e2493653
Gits19 <91568401+Gits19@users.noreply.github.com>
2022-07-20 11:45:07 -0700

    Update index.php

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 |
 */
 
-require __DIR__.'/../laravel2/vendor/autoload.php';
+require __DIR__.'/../Bri/vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -44,7 +44,7 @@ require __DIR__.'/../laravel2/vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../laravel2/bootstrap/app.php';
+$app = require_once __DIR__.'/../Bri/bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
